25518ee03dfab981847306ff5589a4d1f6329a4b
Elmyran <198003617@qq.com>
2025-08-25 21:43:08 +0800

    1

--- tasks/freebies/mail.py
+++ tasks/freebies/mail.py
@@ -9,49 +9,6 @@ from tasks.freebies.assets.assets_freebies_mail import *
 
 
 class MailReward(UI):
-    def _mail_enter(self):
-        """
-        Pages:
-            in: page_menu
-            out: MAIL_CHECK
-        """
-        logger.info('Mail enter')
-        time=Timer(10,count=15).start()
-        for _ in self.loop():
-            if time.reached():
-                raise GameStuckError("Mail enter failed")
-            if self.appear(MAIL_CHECK):
-                logger.info('Mail enter success')
-                break
-            if self.appear_then_click(MAIL_RED_DOT,interval=1,similarity=0.9):
-                continue
-
-
-
-
-
-
-    def _mail_claim(self):
-        """
-        Pages:
-            in: CLAIM_ALL
-            out: CLAIM_ALL_DONE
-        """
-        logger.info('Mail claim all')
-        time=Timer(4,count=8).start()
-        for _ in self.loop():
-            if time.reached():
-                raise GameStuckError("Mail claim all failed")
-            if self.appear(CLAIM_ALL_DONE):
-                break
-            if self.appear_then_click(CLAIM_ALL,interval=1):
-                continue
-
-
-
-
-
-
 
     def handle_mail_reward(self):
         """
@@ -68,6 +25,29 @@ class MailReward(UI):
         self.ui_ensure(page_mail)
         self._mail_claim()
         self.ui_goto_main()
+
+    def _mail_claim(self):
+        """
+        Pages:
+            in: CLAIM_ALL
+            out: CLAIM_ALL_DONE
+        """
+        logger.info('Mail claim all')
+        time=Timer(20,count=20).start()
+        for _ in self.loop():
+            if time.reached():
+                raise GameStuckError("Mail claim all failed")
+            if self.appear(CLAIM_ALL_DONE):
+                break
+            if self.appear_then_click(CLAIM_ALL,interval=0):
+                continue
+
+
+
+
+
+
+
 
 
 
