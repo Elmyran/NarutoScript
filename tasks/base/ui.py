6f1d2972a25cf71a799d47150c98ada1f920cc02
Elmyran <198003617@qq.com>
2025-10-11 20:01:18 +0800

    fix(ui): add wait_until_stable call before checking MAIN_GOTO_CHARACTER appearance

--- tasks/base/ui.py
+++ tasks/base/ui.py
@@ -307,6 +307,7 @@ class UI(MainPage):
             return False
 
         appear = False
+        self.wait_until_stable(MAIN_GOTO_CHARACTER, timer=Timer(0.5, count=1))
         if MAIN_GOTO_CHARACTER.match_template_luma(self.device.image):
             if self.image_color_count(MAIN_GOTO_CHARACTER, color=(235, 235, 235), threshold=234, count=400):
                 appear = True
