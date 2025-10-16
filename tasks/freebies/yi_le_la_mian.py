5f91a6403a7a66637d620f4d0492c2d77f265a61
Elmyran <198003617@qq.com>
2025-10-16 11:28:55 +0800

    fix(tasks): stable claim YiLeLaMian

--- tasks/freebies/yi_le_la_mian.py
+++ tasks/freebies/yi_le_la_mian.py
@@ -1,22 +1,29 @@
 from module.base.timer import Timer
+from tasks.activity.activity_keyword import YiLeWaiMaiKeyword
 from tasks.activity.assets.assets_activity import *
+from tasks.activity.draglist import ACTIVITY_TAB_LIST
 from tasks.freebies.assets.assets_freebies_yi_le_la_mian import *
 from tasks.base.page import page_activity
 from tasks.base.ui import UI
 from datetime import datetime
-from module.logger.logger import logger
+from module.logger import logger
 
 class YiLeLaMian(UI):
     def handle_la_mian(self):
         now = datetime.now() 
         if now.hour<11:
-            logger.info('Not 11am yet, skip claim')
+            logger.info(f'Not 11am yet, skip claim')
             return False
         if self.config.stored.YiLeLaMianFinishCount.is_expired():
             self.config.stored.YiLeLaMianFinishCount.clear()
         if self.config.stored.YiLeLaMianFinishCount.is_full():
             return True
+        self._claim()
+        self.config.stored.YiLeLaMianFinishCount.add()
+        return True
+    def _claim(self):
         self.ui_ensure(page_activity)
+        ACTIVITY_TAB_LIST.search_rows(main=self,keyword=YiLeWaiMaiKeyword)
         time=Timer(10,20).start()
         for _ in  self.loop():
             if time.reached():
@@ -27,8 +34,9 @@ class YiLeLaMian(UI):
                 break
             if self.appear_then_click(RAMEN_CLAIM,interval=1):
                 continue
-        self.ui_goto_main()
-        self.config.stored.YiLeLaMianFinishCount.add()
+        
+        return True
+
 
 
 
