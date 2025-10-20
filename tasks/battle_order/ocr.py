ca27baa7e6bb9ef4cc5a15fc0b6e076e993d1019
Elmyran <198003617@qq.com>
2025-10-20 09:34:38 +0800

    refactor(battle_order/ocr): remove unused ONNXPaddleOcr inheritance

--- tasks/battle_order/ocr.py
+++ tasks/battle_order/ocr.py
@@ -1,7 +1,6 @@
 from module.ocr.ocr import Digit
-from module.ocr.onnxocr.onnx_paddleocr import ONNXPaddleOcr
 
 
-class BattleOrderOcr(ONNXPaddleOcr,Digit):
+class BattleOrderOcr(Digit):
     def format_result(self, result):
         return Digit.format_result(self, result)
