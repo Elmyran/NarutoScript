db900f40fbdeeb1ab4b67b60db681310d868e7c3
Elmyran <198003617@qq.com>
2025-10-22 13:42:58 +0800

    feat(assets): update leaderboard button assets and positions

--- tasks/freebies/assets/assets_freebies_leaderboard.py
+++ tasks/freebies/assets/assets_freebies_leaderboard.py
@@ -25,26 +25,15 @@ LEADER_BOARD_CHECK = ButtonWrapper(
     ),
     en=None,
 )
-LEADER_BOARD_EXIT = ButtonWrapper(
-    name='LEADER_BOARD_EXIT',
-    cn=Button(
-        file='./assets/cn/freebies/leaderboard/LEADER_BOARD_EXIT.png',
-        area=(1175, 0, 1273, 73),
-        search=(1155, 0, 1280, 93),
-        color=(97, 59, 38),
-        button=(1175, 0, 1273, 73),
-    ),
-    en=None,
-)
 LEADER_BOARD_LIKED = ButtonWrapper(
     name='LEADER_BOARD_LIKED',
     cn=[
         Button(
             file='./assets/cn/freebies/leaderboard/LEADER_BOARD_LIKED.png',
-            area=(1178, 347, 1213, 387),
-            search=(1158, 327, 1233, 407),
-            color=(147, 125, 113),
-            button=(1178, 347, 1213, 387),
+            area=(1178, 156, 1213, 193),
+            search=(1158, 136, 1233, 213),
+            color=(164, 147, 93),
+            button=(1178, 156, 1213, 193),
         ),
         Button(
             file='./assets/cn/freebies/leaderboard/LEADER_BOARD_LIKED.2.png',
@@ -55,6 +44,13 @@ LEADER_BOARD_LIKED = ButtonWrapper(
         ),
         Button(
             file='./assets/cn/freebies/leaderboard/LEADER_BOARD_LIKED.3.png',
+            area=(1178, 347, 1213, 387),
+            search=(1158, 327, 1233, 407),
+            color=(147, 125, 113),
+            button=(1178, 347, 1213, 387),
+        ),
+        Button(
+            file='./assets/cn/freebies/leaderboard/LEADER_BOARD_LIKED.4.png',
             area=(1180, 446, 1213, 483),
             search=(1160, 426, 1233, 503),
             color=(100, 141, 126),
@@ -65,13 +61,36 @@ LEADER_BOARD_LIKED = ButtonWrapper(
 )
 LEADER_BOARD_LIKED_FLAG = ButtonWrapper(
     name='LEADER_BOARD_LIKED_FLAG',
-    cn=Button(
-        file='./assets/cn/freebies/leaderboard/LEADER_BOARD_LIKED_FLAG.png',
-        area=(1146, 160, 1236, 217),
-        search=(1126, 140, 1256, 237),
-        color=(179, 116, 58),
-        button=(1146, 160, 1236, 217),
-    ),
+    cn=[
+        Button(
+            file='./assets/cn/freebies/leaderboard/LEADER_BOARD_LIKED_FLAG.png',
+            area=(1146, 160, 1236, 217),
+            search=(1126, 140, 1256, 237),
+            color=(179, 116, 58),
+            button=(1146, 160, 1236, 217),
+        ),
+        Button(
+            file='./assets/cn/freebies/leaderboard/LEADER_BOARD_LIKED_FLAG.2.png',
+            area=(1147, 257, 1237, 316),
+            search=(1127, 237, 1257, 336),
+            color=(155, 111, 113),
+            button=(1147, 257, 1237, 316),
+        ),
+        Button(
+            file='./assets/cn/freebies/leaderboard/LEADER_BOARD_LIKED_FLAG.3.png',
+            area=(1146, 351, 1236, 413),
+            search=(1126, 331, 1256, 433),
+            color=(165, 99, 81),
+            button=(1146, 351, 1236, 413),
+        ),
+        Button(
+            file='./assets/cn/freebies/leaderboard/LEADER_BOARD_LIKED_FLAG.4.png',
+            area=(1147, 448, 1236, 510),
+            search=(1127, 428, 1256, 530),
+            color=(113, 111, 90),
+            button=(1147, 448, 1236, 510),
+        ),
+    ],
     en=None,
 )
 LEADER_BOARD_LIKE_BUTTON = ButtonWrapper(
