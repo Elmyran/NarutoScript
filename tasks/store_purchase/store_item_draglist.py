01a26c54891f09b87c316be07b1798e004886db9
Elmyran <198003617@qq.com>
2025-11-10 21:00:23 +0800

    feat(store-purchase): add insight_row method to ItemDragList

--- tasks/store_purchase/store_item_draglist.py
+++ tasks/store_purchase/store_item_draglist.py
@@ -1,6 +1,9 @@
 import numpy as np
 from module.base.base import ModuleBase
+from module.base.timer import Timer
 from module.base.utils.utils import area_size, random_rectangle_vector_opted
+from module.ocr.keyword import Keyword
+from module.ocr.ocr import OcrResultButton
 from tasks.store_purchase.assets.assets_store_purchase import *
 from module.ui.draggable_list import DraggableList
 from module.logger import logger
@@ -50,6 +53,58 @@ class ItemDragList(DraggableList):
 
         p1, p2 = random_rectangle_vector_opted(vector, box=self.safe_drag_area.button)
         main.device.drag(p1, p2, name=f'{self.name}_DRAG')
+    def insight_row(self, row: Keyword, main: ModuleBase, skip_first_screenshot=True) -> bool:
+        """
+        Args:
+            row:
+            main:
+            skip_first_screenshot:
+
+        Returns:
+            If success
+        """
+        row_index = self.keyword2index(row)
+        if not row_index:
+            logger.warning(f'Insight row {row} but index unknown')
+            return False
+
+        logger.info(f'Insight row: {row}, index={row_index}')
+        last_buttons: set[OcrResultButton] = None
+        bottom_check = Timer(3, count=5).start()
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                main.device.screenshot()
+
+            self.load_rows(main=main)
+
+            # End
+            if self.cur_buttons and self.cur_min <= row_index <= self.cur_max:
+                break
+
+            # Drag pages
+            if row_index < self.cur_min:
+                self.drag_page(self.reverse_direction(self.drag_direction), main=main)
+            elif self.cur_max < row_index:
+                self.drag_page(self.drag_direction, main=main)
+
+            # Wait for bottoming out
+            self.wait_bottom_appear(main, skip_first_screenshot=False)
+            main.wait_until_stable(
+                self.search_button, timer=Timer(0.5, count=3),
+                timeout=Timer(1.5, count=5)
+            )
+            skip_first_screenshot = True
+            if self.cur_buttons and last_buttons == set(self.cur_buttons):
+                if bottom_check.reached():
+                    logger.warning(f'No more rows in {self}')
+                    return False
+            else:
+                bottom_check.reset()
+            last_buttons = set(self.cur_buttons)
+
+        return True
     def search_rows(self, main,keyword):
         self.current_keyword=keyword
         if self.insight_row(keyword, main=main):
