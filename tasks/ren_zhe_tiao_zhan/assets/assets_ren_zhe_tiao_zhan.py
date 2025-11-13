b5d6eb1f30aee879c996842d9ff20a65e2231742
Elmyran <198003617@qq.com>
2025-11-13 11:44:42 +0800

    feat(mi-jing): update ticket recognition logic and assets

--- tasks/ren_zhe_tiao_zhan/assets/assets_ren_zhe_tiao_zhan.py
+++ tasks/ren_zhe_tiao_zhan/assets/assets_ren_zhe_tiao_zhan.py
@@ -76,22 +76,13 @@ MI_JING_FIGHT_CHECK = ButtonWrapper(
 )
 MI_JING_REMAIN_CHALLENGE_TICKET = ButtonWrapper(
     name='MI_JING_REMAIN_CHALLENGE_TICKET',
-    cn=[
-        Button(
-            file='./assets/cn/ren_zhe_tiao_zhan/MI_JING_REMAIN_CHALLENGE_TICKET.png',
-            area=(495, 623, 534, 668),
-            search=(475, 603, 554, 688),
-            color=(31, 30, 20),
-            button=(495, 623, 534, 668),
-        ),
-        Button(
-            file='./assets/cn/ren_zhe_tiao_zhan/MI_JING_REMAIN_CHALLENGE_TICKET.2.png',
-            area=(496, 632, 541, 661),
-            search=(476, 612, 561, 681),
-            color=(33, 35, 14),
-            button=(496, 632, 541, 661),
-        ),
-    ],
+    cn=Button(
+        file='./assets/cn/ren_zhe_tiao_zhan/MI_JING_REMAIN_CHALLENGE_TICKET.png',
+        area=(497, 627, 556, 668),
+        search=(477, 607, 576, 688),
+        color=(34, 34, 17),
+        button=(497, 627, 556, 668),
+    ),
     en=None,
 )
 MI_JING_REWARD_AREA = ButtonWrapper(
