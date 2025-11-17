06c7595d900cd391f1f2a4da445bcb21ae67ab95
Elmyran <198003617@qq.com>
2025-11-17 13:24:48 +0800

    feat(device): add wait time after touch down action

--- module/device/method/maatouch.py
+++ module/device/method/maatouch.py
@@ -267,7 +267,7 @@ class MaaTouch(Connection):
     @retry
     def click_maatouch(self, x, y):
         builder = self.maatouch_builder
-        builder.down(x, y).commit()
+        builder.down(x, y).commit().wait(100)
         builder.up().commit()
         builder.send()
 
