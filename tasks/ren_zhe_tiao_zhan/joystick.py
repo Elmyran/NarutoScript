cd3e30b79b7f1e721a7b49d0bad5ca89974f3d21
Elmyran <198003617@qq.com>
2025-11-18 19:04:37 +0800

    feat(joystick): add uiautomator2 support for multi-finger control

--- tasks/ren_zhe_tiao_zhan/joystick.py
+++ tasks/ren_zhe_tiao_zhan/joystick.py
@@ -44,7 +44,9 @@ class JoystickContact:
         elif method == 'ADB':
             _ = self.main.device.minitouch_builder
             builder = CommandBuilder(self.main.device, contact=1)
-
+        elif method == 'uiautomator2':
+            _ = self.main.device.minitouch_builder
+            builder = CommandBuilder(self.main.device, contact=1)
         else:
             raise ScriptError(f'Control method {method} does not support multi-finger')
         builder.DEFAULT_DELAY = 0.
@@ -57,6 +59,8 @@ class JoystickContact:
         elif method == 'minitouch':
             retry = minitouch_retry
         elif method == 'ADB':
+            retry = minitouch_retry
+        elif method == 'uiautomator2':
             retry = minitouch_retry
         else:
             raise ScriptError(f'Control method {method} does not support multi-finger')
