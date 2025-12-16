358c1a5c7a4494fe951e6993bd81621dea668ed4
Elmyran <198003617@qq.com>
2025-12-16 18:44:47 +0800

    fix(recruit): adjust button area for row selection

--- tasks/recruit/draglist.py
+++ tasks/recruit/draglist.py
@@ -23,6 +23,7 @@ class RecruitDragList(DraggableList):
             if RecruitTabList.select_row(keyword, main=main):
                 logger.info('Successfully selected '+keyword.cn)
     def is_row_selected(self, button, main):
+        button.area=(button.area[0]-50,button.area[1]-50,button.area[2]+50,button.area[3]+50)
         if main.image_color_count(button, color=self.active_color, threshold=240, count=100):
             return True
         return False
