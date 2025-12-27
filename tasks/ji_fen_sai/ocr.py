d0b297d2fa34bfeb48aac54772bed6ebf1113f25
Elmyran <198003617@qq.com>
2025-12-27 12:24:30 +0800

    feat(ji_fen_sai): replace DigitCounter with JiFenSaiDigitCounter

--- tasks/ji_fen_sai/ocr.py
+++ tasks/ji_fen_sai/ocr.py
@@ -1,10 +1,15 @@
 
 
 
-from module.ocr.ocr import Ocr
+from module.ocr.ocr import DigitCounter, Ocr
 
 
 class JiFenSaiOcr(Ocr):
     def after_process(self, result):
+
+        return super().after_process(result)
+class JiFenSaiDigitCounter(DigitCounter):
+    def after_process(self, result):
+        result=result.replace('1V/','11/')
         return super().after_process(result)
     
