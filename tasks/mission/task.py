65730f3a55942b46272891213a3f462aac97e0d9
Elmyran <198003617@qq.com>
2026-09-14 12:28:19 +0800

    fix(tasks/mission): add priority change logs

--- tasks/mission/task.py
+++ tasks/mission/task.py
@@ -53,10 +53,13 @@ class Task:
         TASK_BOX_BLUE.load_search(self.area)
         if TASK_BOX_RED.match_template(image,similarity=0.7):
             self.priority=TaskPriority.RED
+            logger.info(f"优先级: {self.priority}")
         elif TASK_BOX_BLUE.match_template(image,similarity=0.7):
             self.priority=TaskPriority.BLUE
+            logger.info(f"优先级: {self.priority}")
         else:
             self.priority=TaskPriority.GREEN
+            logger.info(f"优先级: {self.priority}")
     def time_parse(self, time: str) -> int:
         """解析时间字符串为分钟数"""
         import re
