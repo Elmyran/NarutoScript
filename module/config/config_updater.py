7a6f228df1de6385bc5fd604665b9dad9d5306aa
Elmyran <198003617@qq.com>
2026-09-15 23:03:19 +0800

    chore: 更换国内PyPI镜像为阿里云镜像

--- module/config/config_updater.py
+++ module/config/config_updater.py
@@ -408,7 +408,7 @@ class ConfigGenerator:
         template = poor_yaml_read(DEPLOY_TEMPLATE)
         cn = {
             'Repository': 'cn',
-            'PypiMirror': 'https://pypi.tuna.tsinghua.edu.cn/simple',
+            'PypiMirror': 'https://mirrors.aliyun.com/pypi/simple',
             'Language': 'zh-CN',
         }
         aidlux = {
